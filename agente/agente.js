8de9a8ab1b31a8fee668a109e79798a561cb51dc
alexbonetti123 <89396286+alexbonetti123@users.noreply.github.com>
2021-09-14 10:15:23 -0300

    Update agente.js

--- agente/agente.js
+++ agente/agente.js
@@ -18,7 +18,6 @@ module.exports = class Agente {
     async moverAgente() {
         let retorno = await this.labirinto.movimentar({x:this.posX, y:this.posY})
         if(retorno !== undefined) {
-            console.log("Retorno: " + retorno.x + ", " + retorno.y)
             this.posX = retorno.x
             this.posY = retorno.y
         }
@@ -32,23 +31,7 @@ module.exports = class Agente {
        this.escreverLabirinto()
        await this.sleep(2000)
        let eficiencia = this.verificarPorcetangemEficiencia()
-       alert("A eficiência do robô foi de " + eficiencia + "%")
-       /*E = eficiencia
-        E > 90 -> LOL, perfeito
-        E >= 70 -> Podia ser muito melhor
-        E >= 50 -> Ta na média
-        E < 50 -> hiper inteligente? kkkkk
-       */
-       if(eficiencia == 100)
-        alert("Classificação: Só não é melhor que um pé de bergamota carregado")
-        else if(eficiencia >= 90 && eficiencia < 100)
-        alert("Tá saindo da jaula o monstro")
-        else if(eficiencia >= 70 && eficiencia < 90)
-        alert("Classificação: Moises, não consegue né?")
-        else if(eficiencia < 70 && eficiencia >= 50)
-        alert("Classificação: Cuidado, hein! Você tá na média")
-        else
-        alert("Classificação: Hiper inteligente? kkkkk")
+       console.log("Eficiência de: " + eficiencia + "%")
         
 
     }
